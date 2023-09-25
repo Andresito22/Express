d5b4a012a401e355b6fd2a06b25f77353b028012
Andres Sarmiento <andresarmientod@gmail.com>
2023-09-25 00:01:35 -0500

    Middleware Entre

---
+++ list-edit-middleware.js
@@ -0,0 +1,18 @@
+function valideTask(req, res, next) {
+    const tarea = req.body;
+  
+    if (!tarea) {
+      return res.status(400).json({ error: "El cuerpo de la solicitud está vacío." });
+    }
+  
+    if (!tarea.id || typeof tarea.isCompleted === "undefined" || !tarea.description) {
+      return res.status(400).json({ error: "La información de la tarea es inválida o faltan atributos." });
+    }
+  
+    next();
+  }
+  
+  module.exports = {
+    valideTask,
+  };
+  
